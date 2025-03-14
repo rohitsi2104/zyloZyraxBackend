bd4c4c22580948709423379e7363783fc27c497f
Rohit <rohitsi2104@gmail.com>
2025-03-14 23:16:04 +0530

    updated zylo subscription v2

--- zylo/urls.py
+++ zylo/urls.py
@@ -52,11 +52,11 @@ urlpatterns = [
     path('testimonials/', get_testimonials, name='get_testimonials'),
     path('callback/', callback_request, name='create-callback-request'),
     path("create-staff/", create_staff_user, name="create_staff_user"),
-    path('create-subscription/', create_subscription, name='create-subscription'),
+    # path('create-subscription/', create_subscription, name='create-subscription'),
     # path('verify-subscribe/', verify_and_subscribe, name='verify-subscribe'),
     # path("create-subscriptions-manually/", subscription_form, name="subscription_form"),
-    path("fetch-subscription/", get_user_subscription, name="get_user_subscription"),
-    path("forgot-password/", forgot_password, name="forgot_password"),
-    path("reset_password/", reset_password, name="reset_password"),
+    # path("fetch-subscription/", get_user_subscription, name="get_user_subscription"),
+    # path("forgot-password/", forgot_password, name="forgot_password"),
+    # path("reset_password/", reset_password, name="reset_password"),
 
 ]
